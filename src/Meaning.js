6f10186d637042f9b91870a4e9530216970ffce2
Jane-Mar <106934020+Jane-Mar@users.noreply.github.com>
2022-07-30 18:08:47 +0100

    Added css to search results

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,18 +2,20 @@ import React from "react";
 
 export default function Meaning(props) {
   return (
-    <div>
-      <h3>{props.meaning.partOfSpeech}</h3>
-      <div>
+    <dl className="Meaning">
+      <dt>{props.meaning.partOfSpeech}</dt>
+      <dd>
         {props.meaning.definitions.map(function (definition, index) {
           return (
             <div key={index}>
               {definition.definition}
-              <em>{definition.example}</em>
+              <div className="mb-3">
+                <i>{definition.example}</i>
+              </div>
             </div>
           );
         })}
-      </div>
-    </div>
+      </dd>
+    </dl>
   );
 }
--- src/Phonetics.js
+++ src/Phonetics.js
@@ -3,10 +3,8 @@ import React from "react";
 export default function Phonetics(props) {
   if (props.phonetics.audio) {
     return (
-      <div className="Phonetics">
-        <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
-          link{" "}
-        </a>
+      <div className="Phonetics ">
+        <audio src={props.phonetics.audio} controls></audio>
         {props.phonetics.text}
       </div>
     );
